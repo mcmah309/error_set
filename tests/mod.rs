674345a184143f2005e2a3bed3733632afdac2eb
mcmah309 <dillonmcmahon79@gmail.com>
2024-04-03 23:23:13 +0000

    test: Implement test from the README

--- tests/mod.rs
+++ tests/mod.rs
@@ -18,6 +18,15 @@ pub mod tests {
 
     #[test]
     fn test() {
-        // let x = Test(1);
+        let magazine_error = MagazineParsingError::MissingNameArg;
+        let crate_error: SetLevelError = magazine_error.into();
+        println!("{:?}", crate_error);
+    
+        let book_error = BookParsingError::MissingDescriptionArg;
+        let crate_error_from_book: SetLevelError = book_error.into();
+        println!("{:?}", crate_error_from_book);
+    
+        let x: Result<(), MagazineParsingError> = Err(MagazineParsingError::MissingNameArg);
+        let y: Result<(), BookParsingError> = x.map_err(Into::into);
     }
 }
