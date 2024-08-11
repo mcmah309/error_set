1bf2d4ac082d3f22ad36436a00b7eddcf48df605
mcmah309 <dillonmcmahon79@email.com>
2024-08-11 23:25:09 +0000

    feat: Add swallow with error

--- src/record.rs
+++ src/record.rs
@@ -40,6 +40,17 @@ where
     fn swallow_debug(self);
     /// Swallows the Result. if [Err], logging as an "trace".
     fn swallow_trace(self);
+
+    /// Swallows the Result. if [Err], logging as an "error" with the result of [f].
+    fn swallow_with_error<F: FnOnce(E) -> D, D: Display>(self, f: F);
+    /// Swallows the Result. if [Err], logging as an "warn" with the result of [f].
+    fn swallow_with_warn<F: FnOnce(E) -> D, D: Display>(self, f: F);
+    /// Swallows the Result. if [Err], logging as an "info" with the result of [f].
+    fn swallow_with_info<F: FnOnce(E) -> D, D: Display>(self, f: F);
+    /// Swallows the Result. if [Err], logging as an "debug" with the result of [f].
+    fn swallow_with_debug<F: FnOnce(E) -> D, D: Display>(self, f: F);
+    /// Swallows the Result. if [Err], logging as an "trace" with the result of [f].
+    fn swallow_with_trace<F: FnOnce(E) -> D, D: Display>(self, f: F);
 }
 
 impl<T, E> RecordContext<T, E> for Result<T, E> {
@@ -162,51 +173,103 @@ where
 {
     #[inline]
     fn swallow_error(self) {
-        if let Err(err) = &self {
+        if let Err(err) = self {
             #[cfg(feature = "tracing")]
-            tracing::error!("{:?}", &err);
+            tracing::error!("{:?}", err);
             #[cfg(feature = "log")]
-            log::error!("{:?}", &err);
+            log::error!("{:?}", err);
         }
     }
 
     #[inline]
     fn swallow_warn(self) {
-        if let Err(err) = &self {
+        if let Err(err) = self {
             #[cfg(feature = "tracing")]
-            tracing::warn!("{:?}", &err);
+            tracing::warn!("{:?}", err);
             #[cfg(feature = "log")]
-            log::warn!("{:?}", &err);
+            log::warn!("{:?}", err);
         }
     }
 
     #[inline]
     fn swallow_info(self) {
-        if let Err(err) = &self {
+        if let Err(err) = self {
             #[cfg(feature = "tracing")]
-            tracing::info!("{:?}", &err);
+            tracing::info!("{:?}", err);
             #[cfg(feature = "log")]
-            log::info!("{:?}", &err);
+            log::info!("{:?}", err);
         }
     }
 
     #[inline]
     fn swallow_debug(self) {
-        if let Err(err) = &self {
+        if let Err(err) = self {
             #[cfg(feature = "tracing")]
-            tracing::debug!("{:?}", &err);
+            tracing::debug!("{:?}", err);
             #[cfg(feature = "log")]
-            log::debug!("{:?}", &err);
+            log::debug!("{:?}", err);
         }
     }
 
     #[inline]
     fn swallow_trace(self) {
-        if let Err(err) = &self {
+        if let Err(err) = self {
             #[cfg(feature = "tracing")]
-            tracing::trace!("{:?}", &err);
+            tracing::trace!("{:?}", err);
             #[cfg(feature = "log")]
-            log::trace!("{:?}", &err);
+            log::trace!("{:?}", err);
+        }
+    }
+
+    //************************************************************************//
+
+    #[inline]
+    fn swallow_with_error<F: FnOnce(E) -> D, D: Display>(self, f: F) {
+        if let Err(err) = self {
+            #[cfg(feature = "tracing")]
+            tracing::error!("{}", f(err));
+            #[cfg(feature = "log")]
+            log::error!("{}", f(err));
+        }
+    }
+
+    #[inline]
+    fn swallow_with_warn<F: FnOnce(E) -> D, D: Display>(self, f: F) {
+        if let Err(err) = self {
+            #[cfg(feature = "tracing")]
+            tracing::warn!("{}", f(err));
+            #[cfg(feature = "log")]
+            log::warn!("{}", f(err));
+        }
+    }
+
+    #[inline]
+    fn swallow_with_info<F: FnOnce(E) -> D, D: Display>(self, f: F) {
+        if let Err(err) = self {
+            #[cfg(feature = "tracing")]
+            tracing::info!("{}", f(err));
+            #[cfg(feature = "log")]
+            log::info!("{}", f(err));
+        }
+    }
+
+    #[inline]
+    fn swallow_with_debug<F: FnOnce(E) -> D, D: Display>(self, f: F) {
+        if let Err(err) = self {
+            #[cfg(feature = "tracing")]
+            tracing::debug!("{}", f(err));
+            #[cfg(feature = "log")]
+            log::debug!("{}", f(err));
+        }
+    }
+
+    #[inline]
+    fn swallow_with_trace<F: FnOnce(E) -> D, D: Display>(self, f: F) {
+        if let Err(err) = self {
+            #[cfg(feature = "tracing")]
+            tracing::trace!("{}", f(err));
+            #[cfg(feature = "log")]
+            log::trace!("{}", f(err));
         }
     }
 }
