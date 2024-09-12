04ccafa029396a8e68a73e6c52dbec8967cfe246
mcmah309 <dillonmcmahon79@gmail.com>
2024-09-12 03:40:06 +0000

    test: Update no_std test

--- test_no_std/main.rs
+++ test_no_std/main.rs
@@ -2,19 +2,23 @@
 #![no_main]
 
 use error_set::{error_set, CoerceResult};
+use core::fmt::Write;
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    readme_example();
+    // readme_example();
+    display();
     exit(0);
 }
+
 
 fn exit(code: i32) -> ! {
     unsafe {
         core::arch::asm!(
-            "mov rax, 60",  // Syscall number for exit
-            "mov rdi, {0}", // Exit code
-            "syscall",      // Trigger the syscall
+            "mov rax, 60",
+            "mov rdi, {0}",
+            "syscall",
             in(reg) code,
             options(noreturn)
         );
@@ -94,4 +98,47 @@ fn readme_example() {
     let result_download_error: Result<(), DownloadError> = Err(io_error).coerce();
     let result_media_error: Result<(), MediaError> = result_download_error.map_err(Into::into);
     assert!(matches!(result_media_error, Err(MediaError::IoError(_))));
+}
+
+// //************************************************************************//
+
+error_set! {
+    AuthError = {
+        A,
+        // #[display("User `{}` with role `{}` does not exist", name, role)] // cannot use format
+        UserDoesNotExist {
+            name: u32,
+            role: u32,
+        },
+        #[display("The provided credentials are invalid")]
+        InvalidCredentials
+    };
+    AuthError2 = {
+        #[display("User does not exist")]
+        UserDoesNotExist {
+            name: u32,
+            role: u32,
+        }
+    };
+}
+
+
+fn display() {
+    // Seems to always seg fault for some reason
+
+    // let x: AuthError2 = AuthError2::UserDoesNotExist {
+    //     name: 1,
+    //     role: 30,
+    // };
+    // let mut buf: heapless::String<300> = heapless::String::new();
+    // write!(buf, "{}", x).unwrap();
+    // assert_eq!(buf.as_str(), "User does not exist");
+    // let x: AuthError = x.into();
+    // let mut buf: heapless::String<300> = heapless::String::new();
+    // write!(buf, "{}", x).unwrap();
+    // assert_eq!(buf.as_str(), "AuthError::UserDoesNotExist");
+    // let x: AuthError = AuthError::InvalidCredentials;
+    // let mut buf: heapless::String<300> = heapless::String::new();
+    // write!(buf, "{}", x).unwrap();
+    // assert_eq!(buf.as_str(), "The provided credentials are invalid");
 }
