44708aa6449a62c950c77d76dc2862ba68e42ab8
mcmah309 <dillonmcmahon79@gmail.com>
2024-12-23 22:25:40 +0000

    feat: err_trail no_std

--- err_trail/src/lib.rs
+++ err_trail/src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(any(test, feature = "tracing", feature = "log")), no_std)]
+
 #[cfg(any(feature = "tracing", feature = "log", feature = "stub"))]
 mod tracing_log_stub;
 #[cfg(any(feature = "tracing", feature = "log", feature = "stub"))]
@@ -7,3 +9,11 @@ pub use  tracing_log_stub::*;
 mod defmt;
 #[cfg(feature = "defmt")]
 pub use defmt::*;
+
+
+#[cfg(all(feature = "tracing", feature = "defmt"))]
+compile_error!("Features 'tracing' and 'defmt' cannot be enabled at the same time.");
+#[cfg(all(feature = "log", feature = "defmt"))]
+compile_error!("Features 'log' and 'defmt' cannot be enabled at the same time.");
+#[cfg(all(feature = "defmt", feature = "stub"))]
+compile_error!("Features 'defmt' and 'context_stub' cannot be enabled at the same time.");
